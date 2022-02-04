5079091abb5b35632e6300344cf5d141d382788c
webcoderspeed <webcoderspeed@gmail.com>
2022-02-04 23:59:18 +0530

    added use hook in react

---
+++ lec-21-my-app-use-state/src/App.tsx
@@ -0,0 +1,87 @@
+import React, {
+  useCallback,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from 'react';
+
+interface Payload {
+  text: string;
+}
+
+interface Todo {
+  id: number;
+  text: string;
+  done: boolean;
+}
+
+type ActionType =
+  | {
+      type: 'ADD_TODO';
+      text: string;
+    }
+  | {
+      type: 'REMOVE_TODO';
+      id: number;
+    };
+
+function App() {
+  const [payload, setPayload] = useState<Payload | null>(null);
+
+  useEffect(() => {
+    fetch('./data.json')
+      .then((res) => res.json())
+      .then((data) => setPayload(data));
+  }, []);
+
+  const [todos, dispatch] = useReducer((todos: Todo[], action: ActionType) => {
+    switch (action.type) {
+      case 'ADD_TODO':
+        return [...todos, { id: todos.length, text: action.text, done: false }];
+      case 'REMOVE_TODO':
+        return todos.filter(({ id }) => id !== action.id);
+      default:
+        return todos;
+    }
+  }, []);
+
+  const newTodoRef = useRef<HTMLInputElement>(null);
+
+  const onAddTodo = useCallback(() => {
+    if (newTodoRef.current) {
+      dispatch({
+        type: 'ADD_TODO',
+        text: newTodoRef.current.value,
+      })
+    }
+  }, []);
+
+  return (
+    <div>
+      {JSON.stringify(payload)}
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button
+              onClick={() =>
+                dispatch({
+                  type: 'REMOVE_TODO',
+                  id: todo.id,
+                })
+              }
+            ></button>
+          </li>
+        ))}
+      </ul>
+
+      <div>
+        <input type='text' ref={newTodoRef} />
+        <button onClick={onAddTodo}>Add Todo</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
